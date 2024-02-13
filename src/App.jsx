b511bb24c3e1dc81fb8946a88a8184d0419e8c0b
kavin <pasupathy.kavinmca@gmail.com>
2024-02-13 06:33:58 +0530

    path

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header'
 import TopContainer from './components/TopContainer'
 import ProjectContainer from './components/ProjectContainer'
-import Skillcontainer from './components/skillcontainer'
+import Skillcontainer from './components/Skillcontainer'
 import EducationContainer from './components/EducationContainer'
 import Contact from './components/Contact'
 export default function App() {
